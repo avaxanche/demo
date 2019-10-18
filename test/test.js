d6d017c4add4fda6ff26451009601b4a82c90d35
Sander Thien <sander.thien@topicus.nl>
2019-10-18 16:52:12 +0200

    google deploy 42

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 import { Selector } from "testcafe"; // first import testcafe selectors
+import { utc } from "moment";
 
 fixture`Getting Started`.page`https://google.nl`; // declare the fixture // specify the start page
 
 //then create a test and place your code there
 test("My first test", async t => {
+  console.log(utc());
+
   await t
     // Use the assertion to check if the actual header text is equal to the expected one
     .expect(Selector("img").withAttribute("alt", "Google").exists)
